b2d2a988a0decae10b683ea010ddc9e57574e1a3
bietiekay <bietiekay@gmail.com>
2016-11-21 07:48:00 +0100

    updated repo

--- install.sh
+++ install.sh
@@ -23,6 +23,6 @@ apt-get -q update && apt-get install -qy \
 npm install -g babel@5
 
 cd /var/tmp/
-git clone https://github.com/oyvinmar/airsonos
+git clone https://github.com/bietiekay/airsonos
 cd airsonos
 npm install -g --unsafe-perm
